e69d149bd971c3b4edf37790d9fc97e6de652aad
Andy Chou <andychchou@gmail.com>
2021-12-17 23:00:01 -0800

    socket for game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,27 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
+import io from 'socket.io-client';
 
 function Game() {
+
+    const ENDPOINT = 'http://localhost:3000'
+
+    useEffect(() => {
+        // const socket = Socket.io("http://localhost:3000", { transports: ['websocket'] });
+
+        const connectionOptions =  {
+            "forceNew" : true,
+            "reconnectionAttempts": "Infinity", 
+            "timeout" : 10000, 
+            "transports" : ["websocket"]
+        }
+        const socket = io.connect(ENDPOINT, connectionOptions)
+
+        socket.on('message', message => {
+            console.log(message);
+        });
+    })
+
     return (
         <div className="container">
             <div className="row">
